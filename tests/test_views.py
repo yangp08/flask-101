f43d0cb58e8ce657f855f8c6e4b75c106f346e28
yangp08 <petchengyang@yahoo.fr>
2021-03-31 14:44:36 +0200

    read function

--- tests/test_views.py
+++ tests/test_views.py
@@ -12,3 +12,9 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, dict)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_product_id_json(self):
+       response = self.client.get("/api/v1/products/1")
+       product = response.json
+       self.assertIsInstance(product, dict)
+       self.assertEquals(product, { 'id': 1, 'name': 'Skello' })
--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,29 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World! Bonjour"
-
-@app.route('/api/v1/products')
-def get_product():
-    PRODUCTS = {
+PRODUCTS = {
     1: { 'id': 1, 'name': 'Skello' },
     2: { 'id': 2, 'name': 'Socialive.tv' },
     3: { 'id': 3, 'name': 'Le Wagon'},
-    }
+}
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/api/v1/products', methods=['GET'])
+def read_all_products():
+    return jsonify(list(PRODUCTS.values())), 200
 
 
-    return jsonify(PRODUCTS)
+@app.route('/api/v1/products/<int:id>', methods=['GET'])
+def read_one_product(id):
+    product = PRODUCTS.get(id)
+    if product is None:
+        abort(404)
+
+    return jsonify(product)
 
