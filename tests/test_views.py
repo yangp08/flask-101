93d0c75ae1fbbd1fbb997da1b2c54c1c2fb19e97
yangp08 <petchengyang@yahoo.fr>
2021-03-31 12:14:20 +0200

    First push to github

---
+++ tests/test_views.py
@@ -0,0 +1,14 @@
+# tests/test_views.py
+from flask_testing import TestCase
+from wsgi import app
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_products_json(self):
+        response = self.client.get("/api/v1/products")
+        products = response.json
+        self.assertIsInstance(products, dict)
+        self.assertGreater(len(products), 2) # 2 is not a mistake here.
--- wsgi.py
+++ wsgi.py
@@ -13,6 +13,7 @@ def get_product():
     PRODUCTS = {
     1: { 'id': 1, 'name': 'Skello' },
     2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'Le Wagon'},
     }
 
 
